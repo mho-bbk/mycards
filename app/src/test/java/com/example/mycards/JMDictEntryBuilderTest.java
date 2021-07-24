da28dc298af01cd92383de36cd8454f3d5e256c1
Kitty Mo Yin Ho <mho01@dcs.bbk.ac.uk>
2021-07-24 10:12:31 +0100

    removed extra files, more tests (fail)

--- app/src/test/java/com/example/mycards/JMDictEntryBuilderTest.java
+++ app/src/test/java/com/example/mycards/JMDictEntryBuilderTest.java
@@ -22,10 +22,57 @@ public class JMDictEntryBuilderTest {
     }
 
     @Test
-    public void singleCommonKanjiKana_singleGloss_getWordTest() {
+    public void singleCommonKanjiKana_singleGlossWord_getWordTest() {
         List<JMDictEntry> eligibleEntries = entryBuilder.getJMDictEntries("CD player");
 
         assertEquals(eligibleEntries.size(), 1);
+
+        JMDictEntry entry = eligibleEntries.get(0);
+
+        assertEquals(entry.getEngDef(), "CD player");
+        assertEquals(entry.getKana().getText(), "シーディープレーヤー");
+        assertEquals(entry.getKanji().getText(), "ＣＤプレーヤー");
+        assertEquals(entry.getWordID(), "1000110");
     }
 
+    @Test
+    public void singleCommonKanjiKana_multiGlossWord_getWordTest() {
+        List<JMDictEntry> eligibleEntries = entryBuilder.getJMDictEntries("T-back");
+
+        assertEquals(eligibleEntries.size(), 1);
+
+        JMDictEntry entry = eligibleEntries.get(0);
+
+        assertEquals(entry.getEngDef(), "T-back");
+        assertEquals(entry.getKana().getText(), "ティーバック");
+        assertEquals(entry.getKanji().getText(), "Ｔバック");
+        assertEquals(entry.getWordID(), "1000170");
+    }
+
+    //multiCommonKanjiKana
+    @Test
+    public void multiCommonKanjiKana_multiGlossWord_getWordTest() {
+        //TODO - find word, or make one up
+    }
+
+    //glossWithMultiWords
+    @Test
+    public void multiCommonKanjiKana_glossWithMultiWords_getWordTest() {
+        //TODO - candidate: obvious - add another word that means obvious to file
+    }
+
+    @Test
+    public void multiSense_glossNotInFirstSense_getWordTest() {
+        List<JMDictEntry> eligibleEntries = entryBuilder.getJMDictEntries("genitals");
+
+        //Reflects num of Japanese words with that definition, not num of Sense in the word
+        assertEquals(eligibleEntries.size(), 1);
+
+        JMDictEntry entry = eligibleEntries.get(0);
+
+        assertEquals(entry.getEngDef(), "genitals");
+        assertEquals(entry.getKana().getText(), "あそこ");
+        assertEquals(entry.getKanji().getText(), "彼処");
+        assertEquals(entry.getWordID(), "1000320");
+    }
 }
